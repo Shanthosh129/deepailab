5bb803a2a61992f72a8ccb2022f50f80ba39d922
Shanthos <pshanthos12@gmail.com>
2024-12-10 01:02:31 +0530

    update on dto

--- src/main/java/com/order/application/dto/SignupRequest.java
+++ src/main/java/com/order/application/dto/SignupRequest.java
@@ -2,9 +2,11 @@ package com.order.application.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
 public class SignupRequest {
+
     private String firstName;
     private String lastName;
     @Email(message = "Invalid email address")
@@ -12,4 +14,36 @@ public class SignupRequest {
     @NotBlank(message = "Password is required")
     private String password;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public @Email(message = "Invalid email address") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Email(message = "Invalid email address") String email) {
+        this.email = email;
+    }
+
+    public @NotBlank(message = "Password is required") String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotBlank(message = "Password is required") String password) {
+        this.password = password;
+    }
 }
+
--- src/main/java/com/order/application/dto/SignupResponse.java
+++ src/main/java/com/order/application/dto/SignupResponse.java
@@ -1,9 +1,27 @@
 package com.order.application.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+
 public class SignupResponse {
     private String message;
     private String email;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
